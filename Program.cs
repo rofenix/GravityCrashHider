3ee64c8f42fa80ea7d52f252ace5c003d17bdf4f
Felipe Andres Yañez Martinez <cdrfenix@hotmail.com>
2024-05-11 04:06:49 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using GravityCrashHider;
+
+partial class Program
+{
+    private static readonly string GravityCrashWindowTitle = "Gravity(tm) Error Handle";
+    private static readonly TimeSpan RefreshRate = TimeSpan.FromSeconds(1);
+    private static bool Hidden = false;
+
+    static void Main(string[] _)
+    {
+        Console.WriteLine("> Gravity crash hider");
+        Console.WriteLine("> Made by Fenix");
+        Console.WriteLine("> Join the iRO Spooky Market: https://discord.gg/PQwpDmRSuM");
+        Console.WriteLine("> Scanner started, dont close this program...");
+        Task.Factory.StartNew(async () => await Start(), TaskCreationOptions.LongRunning);
+        Console.ReadLine();
+    }
+
+    private static async Task Start()
+    {
+        // Scan windows till i find it
+        while (!Hidden)
+        {
+            WinImports.EnumWindows(ScanWindow, 0);
+            await Task.Delay(RefreshRate);
+        }
+    }
+
+    private static bool ScanWindow(nint handle, nint param)
+    {
+        // Is the dialog visible and is the gravity crash handle?
+        if (WinImports.IsWindowVisible(handle) && IsGravityCrashWindow(handle))
+        {
+            // Minimize and hide
+            WinImports.SendMessage(handle, WinImports.WM_SYSCOMMAND, WinImports.SC_MINIMIZE, 0);
+            WinImports.ShowWindow(handle, WinImports.SW_HIDE);
+            Console.WriteLine("> Gravity crash handler detected and is now hidden !");
+            Hidden = true;
+        }
+        return true;
+    }
+
+    private static bool IsGravityCrashWindow(nint handle)
+    {
+        int titleLength = WinImports.GetWindowTextLength(handle);
+        char[] titleBuff = new char[titleLength];
+
+        if (WinImports.GetWindowText(handle, titleBuff, titleLength) <= 0)
+        {
+            return false;
+        }
+
+        var title = new string(titleBuff, 0, titleLength - 1);
+        return title == GravityCrashWindowTitle;
+    }
+}
---
+++ WinImports.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+
+namespace GravityCrashHider;
+
+public partial class WinImports
+{
+    public const uint WM_SYSCOMMAND = 0x0112;
+    public const int SC_MINIMIZE = 0xF020;
+    public const int SW_HIDE = 0;
+
+    public delegate bool EnumWindowsHandler(nint handle, nint lParam);
+
+    [LibraryImport("User32.dll", EntryPoint = "GetWindowTextW", StringMarshalling = StringMarshalling.Utf16)]
+    public static partial int GetWindowText(nint handle, [Out] char[] lpString, int nMaxCount);
+
+    [LibraryImport("user32.dll", EntryPoint = "GetWindowTextLengthA", SetLastError = true)]
+    public static partial int GetWindowTextLength(nint handle);
+
+    [LibraryImport("user32.dll", EntryPoint = "SendMessageA")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool SendMessage(nint handle, uint Msg, nint wParam, nint lParam);
+
+    [LibraryImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool ShowWindow(nint handle, int nCmdShow);
+
+    [LibraryImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool EnumWindows(EnumWindowsHandler lpEnumFunc, nint lParam);
+
+    [LibraryImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool IsWindowVisible(nint handle);
+}
